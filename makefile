3e13524d65c76ebb5aa2e4e925d827f9fbb758ae
Oracle0Zero <paristonh1@gmail.com>
2024-12-17 14:22:32 +0530

    Parser GUI

--- makefile
+++ makefile
@@ -3,11 +3,11 @@ vpath %.c src
 vpath %.h include
 
 CC = g++
-CPPFLAGS = -fopenmp
+CPPFLAGS = -fopenmp -lfreetype
 OUTPUT_OPTION = -o $@
-LOADLIBES = -lGL -lGLU -lSOIL -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi
+LOADLIBES = -lGL -lGLU -lSOIL -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi 
 
-main.exec: main.o shader.o glad.o
+main.exec: main.o shader.o glad.o imgui.o imgui_impl_glfw.o imgui_impl_opengl3.o imgui_draw.o imgui_tables.o imgui_widgets.o imgui_demo.cpp
 	$(CC) $^ $(CPPFLAGS) $(LOADLIBES) -o $@
 	rm -r *.o
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
-#include "../include/shader.h"
 #include <string.h>
 #include <omp.h>
 #include <stack>
 #include <algorithm>
 #include <vector>
+#include <map>
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <ft2build.h>
+#include FT_FREETYPE_H  
+
+#include "../include/shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -16,22 +25,41 @@ void RebindBuffer(GLuint* VAO, GLuint* VBO);
 bool IsOperator(char c);
 int Precedence(char c);
 float PerformOperation(char ope, float number_1, float number_2);
+void RenderText(Shader &s, std::string text, float x, float y, float scale, glm::vec3 color);
 
-GLuint VAO[2];
-GLuint VBO[2];
+GLuint VAO[3];
+GLuint VBO[3];
 
 float x_range = 2;
 float step = 0.01f;
 int total_points = x_range / step;
 float* points = new float[total_points * 2];
 
+float f = 0.0f;
+
 GLuint gridShader, functionShader;
 
-std::string expression_string = "x^3-5*x";
+std::string expression_string = "x^3-15*x+4";
 std::vector<char> expression_chars;
+char input[100];
+
+bool isLastCharNumber = false;
 
 std::stack<float> operands;
 std::stack<float> operators;
+
+struct Character
+{
+	GLuint Texture2D;
+	glm::ivec2 Size;
+	glm::ivec2 Bearing;
+	long int Advance;
+};
+
+std::map<char, Character> Characters;
+
+constexpr float screen_width = 800.0f;
+constexpr float screen_height = 800.0f;
 
 int main()
 {
@@ -57,6 +85,79 @@ int main()
 	glViewport(0, 0, 800, 800);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+	ImGui_ImplOpenGL3_Init();
+
+	//std::cout << function_x(5.0f) << "\n";
+
+
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+	{
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+		return -1;
+	}
+
+	FT_Face face;
+	if (FT_New_Face(ft, "fonts/BitterPro-Medium.ttf", 0, &face))
+	{
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;  
+		return -1;
+	}
+	FT_Set_Pixel_Sizes(face, 0, 48);  
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
+  
+	for (unsigned char c = 0; c < 128; c++)
+	{
+		// load character glyph 
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+		{
+			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+			continue;
+		}
+		// generate texture
+		unsigned int texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_RED,
+			face->glyph->bitmap.width,
+			face->glyph->bitmap.rows,
+			0,
+			GL_RED,
+			GL_UNSIGNED_BYTE,
+			face->glyph->bitmap.buffer
+		);
+		// set texture options
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// now store character for later use
+		Character character = {
+			texture, 
+			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+		Characters.insert(std::pair<char, Character>(c, character));
+	}
+
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
 
 	constexpr float start_x_position  = -1.0f;
 	constexpr float start_y_position  = 1.0f;
@@ -95,8 +196,8 @@ int main()
 	float new_x_coord = 0.0f;
 	float new_y_coord = 0.0f;
 
-	float divider_y = function_x(x_coord);
-	divider_y = divider_y < 0 ? -1*divider_y : divider_y;
+	//float divider_y = function_x(x_coord);
+	//divider_y = divider_y < 0 ? -1*divider_y : divider_y;
 
 	#pragma omp target
 	#pragma omp loop
@@ -104,15 +205,16 @@ int main()
 	{
 		new_x_coord = x_coord + step * (i/2);
 		y_coord = function_x(new_x_coord);
+		//y_coord = new_x_coord;
 		//new_y_coord = y_coord + step * (i/2);
 		
-		if(y_coord != 0)
-		{
-			std::cout << y_coord / divider_y << "\n";
-		}
+		// if(y_coord != 0)
+		// {
+		// 	std::cout << y_coord / divider_y << "\n";
+		// }
 
 		points[i] = static_cast<float>(new_x_coord) / (static_cast<float>(x_range) / 2);
-		points[i + 1] = y_coord;
+		points[i + 1] = y_coord / (static_cast<float>(x_range) / 2);
 
 		//points[i + 1] = static_cast<float>(y_coord) / (static_cast<float>(x_range) / 2);
 	}
@@ -126,7 +228,7 @@ int main()
 	glBindVertexArray(VAO[0]);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
-	glBufferData(GL_ARRAY_BUFFER, (number_of_vertices * 2 * 2) * sizeof(float), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, (number_of_vertices * 2 * 2) * sizeof(float), vertices, GL_DYNAMIC_DRAW);
 
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -145,8 +247,21 @@ int main()
 
 	glBindVertexArray(0);
 
+	// -------------------------------
+
+	glGenVertexArrays(1, &VAO[2]);
+	glGenBuffers(1, &VBO[2]);
+	glBindVertexArray(VAO[2]);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);   
+
 	Shader ourShader("./shaders/vertexShader.glsl", "./shaders/geometryShader.glsl", "./shaders/fragmentShader.glsl");
 	Shader functionShader("./shaders/vertexShader_graph.glsl", "./shaders/fragmentShader_graph.glsl");
+	Shader fontShader("./shaders/vertexShader_Fonts.glsl", "./shaders/fragmentShader_Fonts.glsl");
 
 	//gridShader = Utils::createShaderProgram("./shaders/vertexShader.glsl", "./shaders/geometryShader.glsl", "./shaders/fragmentShader.glsl");
 	//functionShader = Utils::createShaderProgram("./shaders/vertexShader_graph.glsl", "./shaders/fragmentShader_graph.glsl");
@@ -155,6 +270,26 @@ int main()
 	{
 		// input
 		processInput(window);
+
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::SetWindowSize(ImVec2(400, 200));
+		
+		ImGui::InputText("Input", input, IM_ARRAYSIZE(input));
+		if(ImGui::Button("Save", ImVec2(50, 50)))
+		{
+			//std::cout << input << "\n";
+			expression_string = input;
+			x_range = 2;
+			RebindBuffer(VAO, VBO);
+		}
+
+		if(ImGui::SliderFloat("xy-range", &x_range, 2.0f, 200.0f))
+		{
+			RebindBuffer(VAO, VBO);
+		}
 
 		// rendering
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -179,6 +314,25 @@ int main()
 		glDrawArrays(GL_LINE_STRIP, 0, total_points);
 		glBindVertexArray(0);
 
+		// Font Rendering
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); 
+		glm::mat4 projection = glm::ortho(0.0f, 800.0f, 0.0f, 800.0f);
+		fontShader.use();
+    	glUniformMatrix4fv(glGetUniformLocation(fontShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+		std::string font_to_draw = std::to_string(x_range/2);
+		std::string::size_type dot_location = font_to_draw.find(".");
+		std::string final_string = font_to_draw.substr(0, dot_location) + "." + font_to_draw.at(dot_location+1) + font_to_draw.at(dot_location+2);
+		RenderText(fontShader, "-" + final_string, 5.0f, screen_height/2, 0.5f, glm::vec3(0.5, 0.8f, 0.2f));
+		RenderText(fontShader, std::to_string(x_range/2), screen_width-100.0f, screen_height/2, 0.5f, glm::vec3(0.5, 0.8f, 0.2f));
+
+		// Rendering
+		// (Your code clears your framebuffer, renders your other stuff etc.)
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		// (Your code calls glfwSwapBuffers() etc.)
+
+
 		// check and call event and swap buffers
 		glfwPollEvents();
 		glfwSwapBuffers(window);
@@ -188,6 +342,10 @@ int main()
 
 	delete[] vertices;
 	delete[] points;
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
 	return 0;
 }
@@ -209,8 +367,8 @@ void RebindBuffer(GLuint* VAO, GLuint* VBO)
 	float y_coord = 0.0f;
 	float new_x_coord = 0.0f;
 
-	float divider_y = function_x(x_coord);
-	divider_y = divider_y < 0 ? -1*divider_y : divider_y;
+	//float divider_y = function_x(x_coord);
+	//divider_y = divider_y < 0 ? -1*divider_y : divider_y;
 
 	#pragma omp target
 	#pragma omp loop
@@ -218,12 +376,13 @@ void RebindBuffer(GLuint* VAO, GLuint* VBO)
 	{
 		new_x_coord = x_coord + step * (i/2);
 		y_coord = function_x(new_x_coord);
+		//y_coord = new_x_coord;
 		
-		std::cout << "new_x_coord: " << new_x_coord << "\n";
-		std::cout << "y_coord: " << y_coord << "\n";
+		//std::cout << "new_x_coord: " << new_x_coord << "\n";
+		//std::cout << "y_coord: " << y_coord << "\n";
 
 		points[i] = static_cast<float>(new_x_coord) / (static_cast<float>(x_range) / 2);
-		points[i + 1] = y_coord;
+		points[i + 1] = y_coord / (static_cast<float>(x_range) / 2);
 	}
 	
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -280,12 +439,20 @@ void processInput(GLFWwindow* window)
 float function_x(float x)
 {
 	//return glm::sin(x);
-
+	while(!operands.empty())
+	{
+		operands.pop();
+	}
+	while(!operators.empty())
+	{
+		operators.pop();
+	}
 	for(char c : expression_string)
 	{
 		if(c == 'x')
 		{
 			operands.push(x);
+			isLastCharNumber = true;
 
 		}else if(IsOperator(c))
 		{
@@ -299,7 +466,7 @@ float function_x(float x)
 					operators.push(c);
 				}else
 				{
-					while(!operators.empty() && Precedence(c) < Precedence(operators.top()))
+					while(!operators.empty() && Precedence(c) <= Precedence(operators.top()))
 					{
 						char o = operators.top();
 						operators.pop();
@@ -315,10 +482,21 @@ float function_x(float x)
 
 					operators.push(c);
 				}
-			}	
+			}
+			isLastCharNumber = false;
 		}else
 		{
-			operands.push(c - '0');
+			if(isLastCharNumber)
+			{
+				float lastNumber = operands.top();
+				operands.pop();
+				operands.push(lastNumber*10 + (c - '0'));
+			}else
+			{
+				operands.push(c - '0');
+			}
+			//std::cout << operands.top() << "\n";
+			isLastCharNumber = true;
 		}
 	}
 
@@ -339,6 +517,7 @@ float function_x(float x)
 	//std::cout << "Input: " << x << "\n";
 	//std::cout << "Result: " << operands.top() << "\n";
 
+
 	return operands.top();
 	//return x * x;
 }
@@ -347,6 +526,7 @@ bool IsOperator(char c)
 {
 	if(c == '^' || c == '+' || c == '-' || c == '*' || c == '\\')
 	{
+		//std::cout << "Operator: " << c << "\n";
 		return true;
 	}
 
@@ -394,4 +574,48 @@ float PerformOperation(char ope, float number_1, float number_2)
 	}
 
 	return result;
+}
+
+void RenderText(Shader &s, std::string text, float x, float y, float scale, glm::vec3 color)
+{
+    // activate corresponding render state	
+    s.use();
+    glUniform3f(glGetUniformLocation(s.ID, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(VAO[2]);
+
+    // iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        Character ch = Characters[*c];
+
+        float xpos = x + ch.Bearing.x * scale;
+        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        float w = ch.Size.x * scale;
+        float h = ch.Size.y * scale;
+        // update VBO for each character
+        float vertices[6][4] = {
+            { xpos,     ypos + h,   0.0f, 0.0f },            
+            { xpos,     ypos,       0.0f, 1.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+            { xpos + w, ypos + h,   1.0f, 0.0f }           
+        };
+        // render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.Texture2D);
+        // update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, VBO[2]);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); 
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
